d8742f7f5f71c7f9ff794125b3d70c0747dfebb7
GrantODP <grantoranduprez@gmail.com>
2025-05-02 19:46:28 +0200

    User post

--- backend/domain/User.php
+++ backend/domain/User.php
@@ -1,44 +1,69 @@
-<?php 
+<?php
 
-require_once'./backend/db/Database.php';
+require_once './backend/db/Database.php';
+require_once './backend/core/Result.php';
 
-class User {
-    public string $id;
-    public string $name;
-    public string $email;
-    public string $contact;
+class User
+{
+  public string $id;
+  public string $name;
+  public string $email;
+  public string $contact;
+  public string $password;
 
-    public function __construct(string $id, string $name, string $email, string $contact) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->email = $email;
-        $this->contact = $contact;
+  public function __construct(string $name, string $email, string $contact, string $password)
+  {
+    $this->name = $name;
+    $this->email = $email;
+    $this->contact = $contact;
+    $this->password = $password;
+  }
+
+  public static function get(string $user_id): ?User
+  {
+    try {
+      Database::connect();
+
+      $db = Database::db();
+      $stmt = $db->prepare("SELECT * FROM users WHERE user_id = :userid");
+      $stmt->bindValue(':userid', $user_id);
+      $stmt->execute();
+
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      if ($row) {
+        $user =  new User(
+          $row['name'],
+          $row['email'],
+          $row['contact'],
+          $row['password']
+        );
+        $user->id = $row['user_id'];
+        return  $user;
+      }
+    } catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
     }
 
-    public static function get(string $email): ?User {
-        try {
-            Database::connect(); 
-    
-            $db = Database::db();
-            $stmt = $db->prepare("SELECT * FROM users WHERE email = :email");
-            $stmt->bindValue(':email', $email);
-            $stmt->execute();
-    
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($row) {
-                return new User(
-                    $row['user_id'],
-                    $row['name'],
-                    $row['email'],
-                    $row['contact']
-                );
-            }
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-    
-        return null;
+    return null;
+  }
+
+  public function post(): Result
+  {
+
+    try {
+      Database::connect();
+
+      $db = Database::db();
+      $stmt = $db->prepare("INSERT INTO users (name, email, contact, password) VALUES (:name, :email, :contact, :password)");
+      $stmt->execute([
+        ':name' => $this->name,
+        ':email' => $this->email,
+        ':contact' => $this->contact,
+        ':password' => $this->password,
+      ]);
+    } catch (PDOException $e) {
+      return Result::Err("Error: " . $e->getMessage());
     }
+    return Result::Ok(null);
+  }
 }
-
-?>
