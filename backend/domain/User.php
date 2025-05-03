61ddb5c079f65abcbbd6f478da3b504994aa2138
GrantODP <grantoranduprez@gmail.com>
2025-05-03 13:20:37 +0200

    more specific user api operations

--- backend/domain/User.php
+++ backend/domain/User.php
@@ -19,7 +19,7 @@ class User
     $this->password = $password;
   }
 
-  public static function get(string $user_id): ?User
+  public static function get_by_id(string $user_id): ?User
   {
     try {
       Database::connect();
@@ -47,15 +47,36 @@ class User
     return null;
   }
 
-  public static function exists(string $email): bool
+  public function get_by_email(string $email = ""): ?User
   {
-    $db = Database::db();
-    $stmt = $db->prepare("SELECT 1 FROM users WHERE email = :email LIMIT 1");
-    $stmt->execute([':email' => $email]);
-    return (bool) $stmt->fetchColumn();
+    try {
+      Database::connect();
+
+      $db = Database::db();
+      $stmt = $db->prepare("SELECT 1 FROM users WHERE email = :email LIMIT 1");
+      $stmt->bindValue(':email', $email);
+      $stmt->execute();
+
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      if ($row) {
+        $user =  new User(
+          $row['name'],
+          $row['email'],
+          $row['contact'],
+          $row['password']
+        );
+        $user->id = $row['user_id'];
+        return  $user;
+      }
+    } catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+
+    return null;
   }
 
-  public function post(): Result
+
+  public function create(): Result
   {
 
     try {
@@ -68,6 +89,48 @@ class User
         ':email' => $this->email,
         ':contact' => $this->contact,
         ':password' => $this->password,
+      ]);
+    } catch (PDOException $e) {
+      return Result::Err("Error: " . $e->getMessage());
+    }
+    return Result::Ok(null);
+  }
+
+  public static function delete(int $user_id): Result
+  {
+    try {
+      $db = Database::db();
+      $stmt = $db->prepare("DELETE 1 FROM users WHERE user_id = :user_id");
+      $stmt->execute([':user_id' => $user_id]);
+    } catch (PDOException $e) {
+      return Result::Err("Error: " . $e->getMessage());
+    }
+    return Result::Ok(null);
+  }
+
+  public static function update_name(int $user_id, string $name): Result
+  {
+    try {
+      $db = Database::db();
+      $stmt = $db->prepare("UPDATE users SET name = :name WHERE user_id = :user_id");
+      $stmt->execute([
+        ':user_id' => $user_id,
+        ':name' => $name
+      ]);
+    } catch (PDOException $e) {
+      return Result::Err("Error: " . $e->getMessage());
+    }
+    return Result::Ok(null);
+  }
+
+  public static function update_contact(int $user_id, string $name): Result
+  {
+    try {
+      $db = Database::db();
+      $stmt = $db->prepare("UPDATE users SET contact = :contact WHERE user_id = :user_id");
+      $stmt->execute([
+        ':contact' => $user_id,
+        ':name' => $name
       ]);
     } catch (PDOException $e) {
       return Result::Err("Error: " . $e->getMessage());
