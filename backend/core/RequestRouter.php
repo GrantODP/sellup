67a280cd35d489b135744c99ef9de83c75bf3a97
GrantODP <grantoranduprez@gmail.com>
2025-05-03 19:37:16 +0200

    use responder

--- backend/core/RequestRouter.php
+++ backend/core/RequestRouter.php
@@ -1,29 +1,26 @@
-<?php 
+<?php
+require_once './backend/core/Responder.php';
+class ReqRouter
+{
+  private $routes = [];
 
-class ReqRouter {
-    private $routes = [];
-    
-    public function add($method, $path, $controller) {
-        $this->routes[$method][$path] = $controller;        
+  public function add($method, $path, $controller)
+  {
+    $this->routes[$method][$path] = $controller;
+  }
+
+  public function handle($method, $uri)
+  {
+    $path = parse_url($uri, PHP_URL_PATH);
+    $controller = $this->routes[$method][$path] ?? null;
+
+    if (!$controller) {
+      Responder::bad_request("Unknown request");
+      return;
     }
-    
-    public function handle($method, $uri)  {
-        $path = parse_url($uri, PHP_URL_PATH);
-        $controller = $this->routes[$method][$path] ?? null; 
-        
-        if (!$controller) {
-            http_response_code(404);
-            echo json_encode(['error'=> 'Not Found']);
-            return;
-        }
-        
-        [$controller, $function] = $controller;
-        (new $class)->$function();
-        
-    }
-        
-    
+
+    [$controller, $function] = $controller;
+    (new $controller)->$function();
+  }
 }
 
-
-?>
