e9bbc82254da0767b300b998f482c834efd90d3b
GrantODP <grantoranduprez@gmail.com>
2025-05-04 15:55:13 +0200

    updated router post and get operations only

--- backend/core/RequestRouter.php
+++ backend/core/RequestRouter.php
@@ -1,26 +1,55 @@
 <?php
 require_once './backend/core/Responder.php';
-class ReqRouter
+class Router
 {
-  private $routes = [];
+  private $get_routes = [];
+  private $post_routes = [];
 
-  public function add($method, $path, $controller)
+  public function add_post($path, $controller)
   {
-    $this->routes[$method][$path] = $controller;
+    $this->post_routes[$path] = $controller;
+  }
+  public function add_get($path, $controller)
+  {
+    $this->get_routes[$path] = $controller;
   }
 
-  public function handle($method, $uri)
+  public function get()
   {
-    $path = parse_url($uri, PHP_URL_PATH);
-    $controller = $this->routes[$method][$path] ?? null;
+    self::call($this->get_routes);
+  }
+  public function post()
+  {
+    self::call($this->post_routes);
+  }
 
-    if (!$controller) {
+  public function handle()
+  {
+    $method = $_SERVER['REQUEST_METHOD'];
+    if ($method === 'GET') {
+      $this->get();
+    } elseif ($method === 'POST') {
+      $this->post();
+    } elseif ($method === 'PUT') {
+      Responder::bad_request("Unsupported PUT");
+    } elseif ($method === 'DELETE') {
+      Responder::bad_request("Unsupported DELETE");
+    } else {
+      Responder::bad_request("Unknown request " . $method);
+    }
+  }
+  private static function call($router)
+  {
+
+    $uri = $_SERVER['REQUEST_URI'];
+    $path = parse_url($uri, PHP_URL_PATH);
+
+    $controller = $router[$path] ?? null;
+    if ($controller == null) {
       Responder::bad_request("Unknown request");
       return;
     }
 
-    [$controller, $function] = $controller;
-    (new $controller)->$function();
+    call_user_func($controller);
   }
 }
-
