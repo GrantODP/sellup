b6866864fad118935c3b7051ed28ea234d3ff440
GrantODP <grantoranduprez@gmail.com>
2025-05-05 13:32:13 +0200

    catergory functionality

---
+++ backend/domain/Category.php
@@ -0,0 +1,63 @@
+<?php
+require_once './backend/core/Result.php';
+require_once './backend/db/Database.php';
+require_once './backend/util/Util.php';
+
+
+class Category
+{
+  public string $name;
+  public string $description;
+  public string $cat_id;
+
+
+  public function __construct(array $data)
+  {
+    $this->name = $data['name'];
+    $this->description = $data['description'];
+    $this->cat_id = $data['cat_id'];
+  }
+
+
+
+  public static function get_by_id(string $id): ?Category
+  {
+
+    try {
+      Database::connect();
+
+      $db = Database::db();
+      $stmt = $db->prepare("SELECT * FROM category WHERE cat_id = :id LIMIT 1");
+      $stmt->bindValue(':id', $id);
+      $stmt->execute();
+
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      if ($row) {
+        $cat =  new Category($row);
+        return  $cat;
+      }
+    } catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+    return null;
+  }
+
+  public static function get_all(): ?array
+  {
+    try {
+      Database::connect();
+
+      $db = Database::db();
+      $stmt = $db->prepare("SELECT * FROM category");
+      $stmt->execute();
+
+      $cats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      if ($cats) {
+        return  $cats;
+      }
+    } catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+    return null;
+  }
+}
