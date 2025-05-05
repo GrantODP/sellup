c9b510e6dcffa0d3d7fd01bb76e4cb3e651d067b
GrantODP <grantoranduprez@gmail.com>
2025-05-05 13:25:17 +0200

    initial slug generator

--- backend/util/Util.php
+++ backend/util/Util.php
@@ -1,8 +1,26 @@
 <?php
 
-function get_input_json(): array
+
+function get_input_json(): ?array
 {
   $input = file_get_contents(('php://input'));
   $data = json_decode($input, true);
   return $data;
+}
+
+function has_required_keys(array $data, array $keys): bool
+{
+  foreach ($keys as $key) {
+    if (!array_key_exists($key, $data)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function gen_slug(string $title): string
+{
+  $slug = strtolower($title);
+  $slug = trim($slug);
+  return str_replace(' ', '-', $slug);
 }
