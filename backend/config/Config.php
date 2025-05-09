c90644d5c64a5a2f718f316c48b4933e61359f9e
GrantODP <grantoranduprez@gmail.com>
2025-05-09 13:13:41 +0200

    CONFIG rename

--- backend/config/Config.php
+++ backend/config/Config.php
@@ -1,19 +1,22 @@
 <?php
 
-class Config {
-    public static $data = [];
+class C2Config
+{
+  public static $data = [];
 
-    public static function load(): void {
+  public static function load(): void
+  {
 
-        self::$data = require './backend/config/sys_config.php';
-    }
+    self::$data = require './backend/config/sys_config.php';
+  }
 
-    public static function get(string $table, string $key): mixed {
-        return self::$data[$table][$key] ?? null;
-    }
+  public static function get(string $table, string $key): mixed
+  {
+    return self::$data[$table][$key] ?? null;
+  }
 
-    public static function getAll(): array {
-        return self::$data;
-    }
+  public static function getAll(): array
+  {
+    return self::$data;
+  }
 }
-?>
