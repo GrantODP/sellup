b82f283baf18e85097e94ba93da2a58af37dea45
GrantODP <grantoranduprez@gmail.com>
2025-05-09 13:13:05 +0200

    fixed user config

--- backend/db/Database.php
+++ backend/db/Database.php
@@ -1,32 +1,32 @@
-<?php 
+<?php
 
 require_once './backend/config/Config.php';
 
-class Database {
-    private static ?PDO $pdo = null;
+class Database
+{
+  private static ?PDO $pdo = null;
 
-    public static function connect(): void {
-        if (self::$pdo != null) {
-            return;
-        }
+  public static function connect(): void
+  {
+    if (self::$pdo != null) {
+      return;
+    }
 
-       
-            $host = Config::get('database',"host");
-            $user = Config::get('database',"user");
-            $password = Config::get('database',"password");
-            $dbname = Config::get('database',"dbname");
-            $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            self::$pdo = $conn;
-   
+
+    $host = C2Config::get('database', "host");
+    $user = C2Config::get('database', "user");
+    $password = C2Config::get('database', "password");
+    $dbname = C2Config::get('database', "dbname");
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    self::$pdo = $conn;
+  }
+
+  public static function db(): ?PDO
+  {
+    if (self::$pdo === null) {
+      throw new Exception("Database not connected.");
     }
-    
-    public static function db(): ?PDO {
-        if (self::$pdo === null) {
-            throw new Exception("Database not connected.");
-        }
-        return self::$pdo;
-        
-    }
+    return self::$pdo;
+  }
 }
-?>
