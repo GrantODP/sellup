194cf21dbd22211863e3c3dfe7700304297aeb51
Grant Du Preez <71445407+GrantODP@users.noreply.github.com>
2025-06-12 19:42:30 +0200

    Remove keys sys_config.php

--- backend/config/sys_config.php
+++ backend/config/sys_config.php
@@ -1,17 +1,17 @@
 <?php
 $config =  [
   'database' => [
-    'host' => 'sql101.infinityfree.com',
-    'user' => 'if0_39147920',
-    'password' => 'FqA8mqwB58u4',
-    'port' => 3306,
-    'dbname' => 'if0_39147920_c2c',
+    'host' => '',
+    'user' => '',
+    'password' => '',
+    'port' => ,
+    'dbname' => '',
   ],
   'llm' =>
   [
     'gemini' =>
     [
-      'key' => 'AIzaSyA419SLbvV74Lz8_RqIwLhjexf35uws4Jg',
+      'key' => '',
       'model' => 'gemini-2.0-flash',
       'url' => "https://generativelanguage.googleapis.com/v1beta/models/MODEL_PLACE:generateContent?key=API_PLACE",
     ],
